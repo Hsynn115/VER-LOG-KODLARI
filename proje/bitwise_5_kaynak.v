3e9dc3d6e43a1c99cd29c4e758cfb17695935423
Hsynn115 <hsyncicek115@gmail.com>
2025-12-20 18:55:30 +0300

    Update bitwise_5_kaynak.v

--- proje/bitwise_5_kaynak.v
+++ proje/bitwise_5_kaynak.v
@@ -7,21 +7,21 @@ module bitwise_operators(
  	reg [5:0] y = 0;        // 6bit variable
     reg [5:0] result = 0;   // 6bit variable
 	
-	// Procedure used to continuously monitor 'x', 'y', and 'result'
+
 	initial begin
       $monitor("MON x=%b, y=%b, result=%b", x, y, result);
 	end
 	
-	// Procedure used to generate stimulus
+	
   // burada da kod başlıyor artık 
 	initial begin
     // görüğün gibi bu and ve nand oporatörleridir, diğerleride aynı onlara bakarsın
-        #1;  // wait some time between examples
+        #1;  
         x = 6'b00_0101;
 	    y = 6'b11_0001;
         result = x & y; // AND
        
-        #1;  // Use the same values for x and y from above (reg stores the value)
+        #1;  
         result = ~(x & y); // NAND Try: x ~& y to see what happens
 	    
         #1;
@@ -41,9 +41,10 @@ module bitwise_operators(
         result = x ~^ y; // NXOR
 	    
         #1
-        x = y; // This should make all bits 1
+        x = y; 
         result = ~(x ^ y); // NXOR
 	end
 
 endmodule
+
 
